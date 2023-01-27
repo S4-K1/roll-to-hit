7276cd8afe0a3015ac17799eac40e8a858641e39
S4-K1 <hardcastle.riley@gmail.com>
2023-01-27 16:49:09 +0000

    Setting up the chat box

--- src/App.js
+++ src/App.js
@@ -7,17 +7,25 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Roll to Attack</h1>
-        <div>
-          <Board />
+        <div className="mainContainer">
+          <div className="mainGame">
+            <div>
+              <Board />
+            </div>
+            <div className="diceContainer">
+              <DiceRoller dice={4} />
+              <DiceRoller dice={6} />
+              <DiceRoller dice={8} />
+              <DiceRoller dice={10} />
+              <DiceRoller dice={12} />
+              <DiceRoller dice={20}/>
+            </div>
+          </div>
+          <div className="rightSideMenu">
+            <p>Chat box here</p>
+          </div>
         </div>
-        <div className="diceContainer">
-          <DiceRoller dice={4} />
-          <DiceRoller dice={6} />
-          <DiceRoller dice={8} />
-          <DiceRoller dice={10} />
-          <DiceRoller dice={12} />
-          <DiceRoller dice={20}/>
-        </div>
+        
       </header>
     </div>
   );
