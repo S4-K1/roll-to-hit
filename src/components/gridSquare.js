e4379f4d7142a888e222913dea69f024e1d451e8
S4-K1 <hardcastle.riley@gmail.com>
2023-03-08 17:30:46 +0000

    Displays example token on board

--- src/components/gridSquare.js
+++ src/components/gridSquare.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import standInImage from '../resources/d20Icon.png';
+import icon from '../resources/exampleIcon.png';
 import './css/gridSquare.css';
 
 
@@ -9,7 +9,7 @@ class GridSquare extends React.Component {
   render() {
     if (this.props.displayIcon) {
       const StyledDiv = styled.div`
-        background-image: url(${standInImage});
+        background-image: url(${icon});
         background-size: cover;
       `;
       return (
