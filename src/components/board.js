aa7dc309132fcbe7857f97b167e87c5b8c6e9a99
S4-K1 <hardcastle.riley@gmail.com>
2023-04-06 15:47:46 +0100

    Pain

--- src/components/board.js
+++ src/components/board.js
@@ -4,40 +4,31 @@ import TokenSquare from './tokenSquare';
 import PropTypes from 'prop-types';
 import './css/board.css';
 
-function createBoard(props, x, y) {
-  function openTokenMenu() {
-    props.openTokenMenu();
-  }
-
+function createBoard(x, y) {
   const board = [];
-  for (let column = 0; column < y; column++) {
+  for (let row = 0; row < x; row++) {
     board.push([]);
-    for (let row = 0; row < x; row++) {
-      const key = `${column}${row}`;
-      if (key === '25') {
-        board[column].push(
-          <TokenSquare
-            key={key}
-            keyProp={key}
-            openTokenMenu={openTokenMenu}
-          />,
-        );
-      } else {
-        board[column].push(
-          <GridSquare key={key}/>,
-        );
-      }
+    for (let column = 0; column < y; column++) {
+      const key = `${row}${column}`;
+      board[row].push(
+        <GridSquare key={key}/>,
+      );
     }
   }
   return board;
 }
 
 function Board(props) {
-  const board = createBoard(props, 10, 10);
+  const board = createBoard(10, 10);
   // TODO Change these to function parameters when implementing changing board size
   return (
-    <div className="board">
+    <div>
+      <div>
+       <TokenSquare openTokenMenu={props.openTokenMenu} position={[1, 2]} />
+      </div>
+      <div className="board">
         {board}
+      </div>
     </div>
   );
 }
--- src/components/tokenSquare.js
+++ src/components/tokenSquare.js
@@ -4,14 +4,25 @@ import PropTypes from 'prop-types';
 import './css/gridSquare.css';
 
 
-class TokenSquare extends React.Component {
+export default class TokenSquare extends React.Component {
   render() {
     const openTokenMenu = () => {
+      console.log('Here stupid', this.props.position);
       this.props.openTokenMenu();
+    };
+
+    const convertToMargin = (positionNum) => {
+      positionNum = positionNum * 25;
+      return positionNum;
     };
 
     return (
       <img
+        style={{
+          position: 'absolute',
+          marginLeft: `${convertToMargin(this.props.position[0]) - 150}px`,
+          marginTop: `${convertToMargin(this.props.position[1]) - 25}px`,
+        }}
         className='grid-square'
         src={icon}
         onClick={openTokenMenu}
@@ -21,8 +32,6 @@ class TokenSquare extends React.Component {
 }
 
 TokenSquare.propTypes = {
-  keyProp: PropTypes.string,
   openTokenMenu: PropTypes.func,
+  position: PropTypes.array,
 };
-
-export default TokenSquare;
