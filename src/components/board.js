7a0e66a8be7d8ad297c319253d66c811ddfa4c05
S4-K1 <hardcastle.riley@gmail.com>
2023-04-08 15:36:32 +0100

    Close to movement, I need a break

--- src/components/board.js
+++ src/components/board.js
@@ -5,6 +5,8 @@ import PropTypes from 'prop-types';
 
 function Board(props) {
   const [tokenSelected, setTokenSelected] = useState(false);
+  const [xPosition, setXPosition] = useState(4);
+  const [yPosition, setYPosition] = useState(3);
 
   function openTokenMenu() {
     props.openTokenMenu();
@@ -16,9 +18,48 @@ function Board(props) {
     setTokenSelected(false);
   }
 
+  function moveTokenLeft() {
+    setXPosition(xPosition - 1);
+  }
+
+  function moveTokenUp() {
+    setYPosition(2);
+    console.log(yPosition);
+  }
+
+  function moveTokenRight() {
+    setXPosition(xPosition + 1);
+  }
+
+  function moveTokenDown() {
+    setYPosition(yPosition - 1);
+  }
+
+  function handleArrows(event) {
+    switch (event.keyCode) {
+      case 37:
+        console.log('left');
+        moveTokenLeft();
+        break;
+      case 38:
+        console.log('up');
+        moveTokenUp();
+        break;
+      case 39:
+        console.log('right');
+        moveTokenRight();
+        break;
+      case 40:
+        console.log('down');
+        moveTokenDown();
+        break;
+    }
+  }
+
+  document.addEventListener('keydown', handleArrows);
   return (
     <div>
-      <TokenSquare openTokenMenu={openTokenMenu} position={[3, 9]} selected={tokenSelected}/>
+      <TokenSquare openTokenMenu={openTokenMenu} position={[xPosition, yPosition]} selected={tokenSelected}/>
       <GridLayer x={10} y={10} closeTokenMenu={closeTokenMenu} />
     </div>
   );
